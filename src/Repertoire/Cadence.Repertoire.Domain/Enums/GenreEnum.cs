3eedf174fbddc5ba4bc2044439191b1519462ee7
Gabriel Lima <g.lima2136@gmail.com>
2026-08-07 01:30:10 -0400

    fix: fix GenreEnum

--- src/Repertoire/Cadence.Repertoire.Domain/Enums/GenreEnum.cs
+++ src/Repertoire/Cadence.Repertoire.Domain/Enums/GenreEnum.cs
@@ -2,8 +2,13 @@
 {
     public enum GenreEnum
     {
-        Male = 0,
-        Female = 1,
-        NA = 2
+        Baroque = 0,
+        Classical = 1,
+        Romantic = 2,
+        Modern = 3,
+        Contemporary = 4,
+        Jazz = 5,
+        Folk = 6,
+        Other = 7
     }
 }
