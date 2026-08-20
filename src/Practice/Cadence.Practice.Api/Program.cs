6c13b03ec488d78549f31645e8a43fc4ee52a10d
Gabriel Lima <g.lima2136@gmail.com>
2026-08-20 03:21:31 -0400

    feat: add Practice api layer

--- src/Practice/Cadence.Practice.Api/Program.cs
+++ src/Practice/Cadence.Practice.Api/Program.cs
@@ -1,5 +1,17 @@
+using Cadence.Practice.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<PracticeDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("Practice") ?? "Data Source=practice.db"));
+
 var app = builder.Build();
+
+using (IServiceScope migrationScope = app.Services.CreateScope())
+{
+    migrationScope.ServiceProvider.GetRequiredService<PracticeDbContext>().Database.Migrate();
+}
 
 app.MapGet("/", () => "Hello World!");
 
